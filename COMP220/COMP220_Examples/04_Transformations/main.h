cfaf8bbd3da87e7729db1cc42077cb5fdd2be0ea
Steven <steven.cowie@hotmail.co.uk>
2017-10-20 12:28:59 +0100

    cOMMIT

---
+++ COMP220/COMP220_Examples/04_Transformations/main.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <fstream>
+
+#include <SDL.h>
+#include <GL\glew.h>
+#include <SDL_opengl.h>
+
+#include <glm\glm.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm\gtx\transform.hpp>
+#include <glm\gtc\type_ptr.hpp>
+
+using namespace glm;
